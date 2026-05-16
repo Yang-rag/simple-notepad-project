4ae11ff4f406c8cab461c51b58f405ccf9190159
Yiran Yang <yy12699@auca.kg>
2026-05-17 00:01:48 +0600

    add basic things

--- notepad_exception.h
+++ notepad_exception.h
@@ -1,3 +1,10 @@
-//
-// Created by 27091 on 2026/5/16.
-//
+pragma once
+#include <stdexcept>
+#include <string>
+ 
+// Base exception for all notepad errors
+class notepad_exception : public std::runtime_error {
+public:
+    explicit notepad_exception(const std::string& message)
+        : std::runtime_error(message) {}
+};
