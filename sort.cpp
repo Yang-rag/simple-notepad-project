77596ff5ed7462b316e1b36f23ad0b773218da50
Yiran Yang <yy12699@auca.kg>
2026-05-17 12:30:36 +0600

    complete sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,3 +1,16 @@
-//
-// Created by 27091 on 2026/5/16.
-//
+/#include "sort.h"
+#include <QStringList>
+#include <algorithm>
+
+namespace Sort {
+
+    QString sortLines(const QString& text, bool ascending) {
+        QStringList lines = text.split('\n');
+        if (ascending)
+            std::sort(lines.begin(), lines.end());
+        else
+            std::sort(lines.begin(), lines.end(), std::greater<QString>());
+        return lines.join('\n');
+    }
+
+} // namespace Sort
