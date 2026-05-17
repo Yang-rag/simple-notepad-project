01a651d77c29fba87af9f0ff7babf924e7c5ab80
Yiran Yang <yy12699@auca.kg>
2026-05-17 11:53:36 +0600

    complete uppercase and lowercase and capitalize

--- text_transform.cpp
+++ text_transform.cpp
@@ -1,3 +1,26 @@
-//
-// Created by 27091 on 2026/5/16.
-//
+#include "text_transform.h"
+
+namespace TextTransform {
+
+    QString toUpperCase(const QString& text) {
+        return text.toUpper();
+    }
+
+    QString toLowerCase(const QString& text) {
+        return text.toLower();
+    }
+
+    QString toCapitalize(const QString& text) {
+        QString result = text.toLower();
+        bool newWord = true;
+        for (int i = 0; i < result.size(); ++i) {
+            if (result[i].isSpace()) {
+                newWord = true;
+            } else if (newWord) {
+                result[i] = result[i].toUpper();
+                newWord = false;
+            }
+        }
+        return result;
+    }
+
