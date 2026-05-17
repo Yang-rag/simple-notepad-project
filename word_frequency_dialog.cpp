d9930b3ac940ee9bc0bc6a0380187c80f6c09922
Yiran Yang <yy12699@auca.kg>
2026-05-17 16:05:28 +0600

    complete word_frequency_dialog.cpp

--- word_frequency_dialog.cpp
+++ word_frequency_dialog.cpp
@@ -1,3 +1,21 @@
-//
-// Created by 27091 on 2026/5/16.
-//
+#include "word_frequency_dialog.h"
+#include "ui_word_frequency_dialog.h"
+#include <QRegularExpression>
+#include <map>
+#include <vector>
+#include <algorithm>
+ 
+WordFrequencyDialog::WordFrequencyDialog(const QString& text, QWidget* parent)
+    : QDialog(parent), ui(new Ui::WordFrequencyDialog)
+{
+    ui->setupUi(this);
+    connect(ui->closeBtn, &QPushButton::clicked, this, &QDialog::accept);
+ 
+    // Count word frequencies
+    std::map<QString, int> freq;
+    QRegularExpression wordRe("[A-Za-z]+");
+    auto it = wordRe.globalMatch(text);
+    while (it.hasNext()) {
+        QString word = it.next().captured().toLower();
+        freq[word]++;
+    }
