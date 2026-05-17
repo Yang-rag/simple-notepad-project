d5e43c7639a538f39255474b960fce8c9ee31ff0
Yiran Yang <yy12699@auca.kg>
2026-05-17 16:19:35 +0600

    complete line_number_area.cpp

--- line_number_area.cpp
+++ line_number_area.cpp
@@ -1,3 +1,14 @@
-//
-// Created by 27091 on 2026/5/16.
-//
+#include "line_number_area.h"
+#include <QPainter>
+#include <QPaintEvent>
+
+LineNumberArea::LineNumberArea(QWidget* editor, QWidget* parent)
+    : QWidget(parent)
+{
+    Q_UNUSED(editor);
+    setFixedWidth(45);
+    setAutoFillBackground(true);
+    QPalette pal = palette();
+    pal.setColor(QPalette::Window, QColor(45, 45, 45));
+    setPalette(pal);
+}
